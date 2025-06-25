d77701f9a3e57f93b942d0098ebaaaa57e54d51b
Chungyul <chungyul.lee@deliveryhero.com>
2025-06-25 10:42:06 +0200

    test: disable unnecessary test

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -4,11 +4,13 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeT
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
+    @Disabled
     fun `전진 정지`() {
         assertRandomNumberInRangeTest(
             {
