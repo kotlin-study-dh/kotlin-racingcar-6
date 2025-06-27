025c37b4661427ce555387b77cd6b8def8c20118
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-27 15:45:56 +0200

    feat: add function to move cars randomly

--- src/main/kotlin/racingcar/GameManager.kt
+++ src/main/kotlin/racingcar/GameManager.kt
@@ -6,4 +6,13 @@ import racingcar.domain.Car
 class GameManager(
     val cars: List<Car>,
 ) {
+
+    fun randomMoveCars(): GameManager {
+        val movedCars = cars.map { car ->
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            if (randomNumber < 4) car
+            else car.move()
+        }
+        return GameManager(movedCars)
+    }
 }
