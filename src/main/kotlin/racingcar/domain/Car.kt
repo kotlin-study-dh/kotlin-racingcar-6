87c41a1d4f6df64653096bec60a1d6ae124e12f5
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-27 16:13:54 +0200

    feat: add validation for name length

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -4,6 +4,9 @@ data class Car(
     val name: String,
     val position: Int = 0
 ) {
+    init {
+        require(name.length <= 5) { "Car name must be at least 5 characters" }
+    }
 
     fun move() = this.copy(name, this.position + 1)
 }
