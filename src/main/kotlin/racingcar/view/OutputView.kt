d33f9a9041e097d6027b0cb81d3d0b05f7d928bd
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-27 16:57:18 +0200

    feat: add prompt space

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -2,7 +2,7 @@ package racingcar.view
 
 import racingcar.domain.Car
 
-fun printExecutionResultPrompt() = println("실행 결과")
+fun printExecutionResultPrompt() = println("\n실행 결과")
 
 fun printExecutionResult(cars: List<Car>) {
     cars.forEach {
