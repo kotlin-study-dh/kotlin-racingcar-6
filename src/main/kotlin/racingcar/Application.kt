0739601bf1f71b96606d5f29b89de8b637805415
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-27 16:58:37 +0200

    feat: collect function to run

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import racingcar.domain.Car
+import racingcar.view.printExecutionResult
+import racingcar.view.printExecutionResultPrompt
+import racingcar.view.printWinners
+import racingcar.view.readCarsName
+import racingcar.view.readTryCount
+
 fun main() {
-    // TODO: 프로그램 구현
+    val cars: List<Car> = readCarsName().map { Car(it) }
+    val tryCount = readTryCount()
+    var gameManager = GameManager(cars)
+
+    printExecutionResultPrompt()
+    repeat(tryCount) {
+        gameManager = gameManager.randomMoveCars(RandomGenerator)
+        printExecutionResult(gameManager.cars)
+    }
+    printWinners(gameManager.decideWinners())
 }
