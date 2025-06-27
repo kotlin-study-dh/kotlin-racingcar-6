1c7920bb638d2b819de02121dbabb761c6057f9a
hyeonji.lee <hyeonji.lee@deliveryhero.com>
2025-06-27 12:04:02 +0200

    feat: add game logic

--- src/main/kotlin/racingcar/RacingCarGame.kt
+++ src/main/kotlin/racingcar/RacingCarGame.kt
@@ -5,16 +5,15 @@ class RacingCarGame {
     val outputView = OutputView()
 
     fun start() {
+        val cars = Cars.of(inputView.readNames())
+        val ridingCount = RidingCount(inputView.readRidingCount())
+        val movementRule = MovementRule(RandomNumberGenerator())
 
-    }
-
-    private inline fun <T> retry(crossinline block: () -> T): T {
-        while (true) {
-            try {
-                return block()
-            } catch (e: Exception) {
-                println("[ERROR] ${e.message}")
-            }
+        outputView.printResultTitle()
+        for (i in 1..ridingCount.count) {
+            cars.move(movementRule)
+            outputView.printRacing(cars)
         }
+        outputView.printWinners(cars.findFarestCars())
     }
 }
