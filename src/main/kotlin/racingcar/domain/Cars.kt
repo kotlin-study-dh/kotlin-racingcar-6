4d9665efff49f50c28b7f53ce0d02d8dfe7a7f9e
hyeonji.lee <hyeonji.lee@deliveryhero.com>
2025-06-28 16:35:35 +0200

    refactor: use distance value in cars

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -14,33 +14,25 @@ class Cars(val cars: List<Car>) {
         cars.forEach { car -> car.move(movementRule.shouldMove()) }
     }
 
-    fun findFarthestCars(): MutableSet<Car> {
-        val farthestCars: MutableSet<Car> = mutableSetOf()
-        cars.forEach { car ->
-            car.addFartherCar(farthestCars)
-        }
-        return farthestCars
-    }
-
-    private fun Car.addFartherCar(farthestCars: MutableSet<Car>) {
-        farthestCars.forEach { car ->
-            if (car.isFartherThan(this)) {
-                farthestCars.remove(car)
-                farthestCars.add(this)
-            } else if (car.isSameDistance(this)) {
-                farthestCars.add(this)
+    fun findFarthestCars(): Set<Car> {
+        var maxDistance = Distance(0)
+        for (car in cars) {
+            if (car.distance.isBiggerThan(maxDistance)) {
+                maxDistance = car.distance
             }
         }
+        return cars.filter { it.distance == maxDistance }.toSet()
     }
 
     override fun equals(other: Any?): Boolean {
-        if (this===other) return true
-        if (javaClass!=other?.javaClass) return false
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
         other as Cars
 
-        return cars==other.cars
+        return cars == other.cars
     }
+
     override fun hashCode(): Int {
         return cars.hashCode()
     }
