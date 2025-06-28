4312515468adc5ec4cfd4bc06765aab6cb85bafd
hyeonji.lee <hyeonji.lee@deliveryhero.com>
2025-06-28 20:30:01 +0200

    refactor: fix condition of exception

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -6,7 +6,10 @@ class InputView() {
 
     fun readNames(): List<String> {
         println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-        val names = readLine() ?: throw IllegalArgumentException("Name are required")
+        val names = readLine()
+        if (names.isNullOrBlank()) {
+            throw IllegalArgumentException("Name are required")
+        }
         return names.split(",").map { it -> it.trim() }
     }
 
