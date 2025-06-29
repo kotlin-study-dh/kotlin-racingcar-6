85d1998a696586ac7e5a9eebdaae29cf1e41d6fa
Gibeom Nam <rlqja0523@naver.com>
2025-06-29 13:49:13 +0200

    refactor: change cars property to private and expose a read-only version

--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -5,19 +5,21 @@ import racingcar.generator.NumberGenerator
 class Cars(
     carNames: List<String>,
 ) {
+    private val _cars: List<Car>
     val cars: List<Car>
+        get() = _cars.toList()
 
     init {
         require(carNames.size <= MAX_CAR_COUNT) { "Car count must be less than or equal to $MAX_CAR_COUNT" }
         require(carNames.distinct().size == carNames.size) { "Car names must be unique" }
-        cars = carNames.map { Car(it) }
+        _cars = carNames.map { Car(it) }
     }
 
-    fun moveAll(numberGenerator: NumberGenerator) = cars.forEach { it.move(numberGenerator) }
+    fun moveAll(numberGenerator: NumberGenerator) = _cars.forEach { it.move(numberGenerator) }
 
     fun getWinners(): List<Car> {
-        val maxPosition = cars.maxOf { it.position }
-        return cars.filter { it.position == maxPosition }
+        val maxPosition = _cars.maxOf { it.position }
+        return _cars.filter { it.position == maxPosition }
     }
 
     companion object {
