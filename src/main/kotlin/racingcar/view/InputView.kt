4a82f2a9f9a266a6c30db8bbc5bd786d19dfae9b
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-30 11:38:13 +0200

    feat: add validation for empty car name

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -4,7 +4,10 @@ import camp.nextstep.edu.missionutils.Console
 
 fun readCarsName(): List<String> {
     println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
-    return Console.readLine().replace(" ", "").split(",")
+    return Console.readLine()
+        .also { require(it.isNotBlank()) { "Car name is empty" } }
+        .replace(" ", "")
+        .split(",")
 }
 
 fun readTryCount(): Int {
