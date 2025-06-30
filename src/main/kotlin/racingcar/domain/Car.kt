0e0a2c9d4dd0342d332fd9a450a241728a34af2a
SanghyeokKim <39580658+tkdgur0906@users.noreply.github.com>
2025-06-30 11:22:31 +0200

    refactor: change error message

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -5,7 +5,7 @@ data class Car(
     val position: Int = 0
 ) {
     init {
-        require(name.length <= 5) { "Car name must be at least 5 characters" }
+        require(name.length <= 5) { "Car name must be 5 or less characters" }
     }
 
     fun move() = this.copy(name, this.position + 1)
